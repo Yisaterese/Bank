87ab48fa3efdb6de19155b1793b7beda76e96fce
Yisaterese <teresejosephyisa@gmail.com>
2024-02-20 21:39:18 +0100

    added code

--- src/apps/Bank.java
+++ src/apps/Bank.java
@@ -9,8 +9,10 @@ public class Bank {
     private final ArrayList<Account> accounts = new ArrayList<>();
 
     Account account1 = new Account("hannah", "2345",  "3456456789");
+    Account account2 = new Account("hannah", "2345",  "1234567891");
     public Bank() {
         accounts.add(account1);
+        accounts.add(account2);
     }
 
     public void deposit(BigDecimal depositAmount, String number) throws Exception {
@@ -25,15 +27,18 @@ public class Bank {
         return account1.checkBalance("2345");
     }
 
-    public void Withdraw(BigDecimal amount, String accountNumber, String pin)throws Exception {
+    public void Withdraw(BigDecimal amount, String userAccount, String pin)throws Exception {
         account1.pinIsEqualToLengthOfFour(pin);
-        account1.accountNumberIsValid(accountNumber);
+        account1.accountNumberIsValid(userAccount);
         for (Account account : accounts) {
-            if (account.getAccountNumber().equals(accountNumber))
+            if (isEqualsAccountNumber(userAccount))
                 if (getCompareTo(amount) > 0)
-                    account1.withdraw(amount, "2345");
-                else throw new InsufficientFundsException("Insufficient funds in your account");
+                    account.withdraw(amount, pin);
         }
+    }
+
+    private boolean isEqualsAccountNumber(String accountNumber) {
+        return account1.getAccountNumber().equals(accountNumber);
     }
 
     private int getCompareTo(BigDecimal amount) throws Exception {
@@ -45,8 +50,9 @@ public class Bank {
         account1.accountNumberIsValid(userAccount); 
         account1.accountNumberIsValid(recipientAccount);
         getCompareTo(new BigDecimal(String.valueOf(transferAmount)));
-
-        
+        isEqualsAccountNumber(userAccount);
+        account1.withdraw(transferAmount,userPin);
+        account2.deposit(transferAmount);
     }
 }
 
--- src/apps/BankTest.java
+++ src/apps/BankTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,14 +35,14 @@ public class BankTest {
 
     @Test
     public void accountCanMakeTransfer()throws Exception{
-        bank.deposit(new BigDecimal("2312"), "3456456789");
-        assertEquals(new BigDecimal("2312"), bank.checkBalance("3456456789","2345"));
-        bank.Withdraw(new BigDecimal("2000"), "3456456789", "2345");
-        assertEquals(new BigDecimal("312"), bank.checkBalance("456456789", "2345"));
         bank.deposit(new BigDecimal("2000"), "3456456789");
-        assertEquals(new BigDecimal("2312"), bank.checkBalance("3456456789", "2345"));
-        bank.transfer(new BigDecimal("312"),  "3456456789", "12345678910", "2345");
-        assertEquals(new BigDecimal("2000"),  bank.checkBalance("3456456789", "2345"));
+        assertEquals(new BigDecimal("2000"), bank.checkBalance("3456456789","2345"));
+        bank.Withdraw(new BigDecimal("1000"), "3456456789", "2345");
+        assertEquals(new BigDecimal("1000"), bank.checkBalance("456456789", "2345"));
+        bank.deposit(new BigDecimal("2000"), "3456456789");
+        assertEquals(new BigDecimal("3000"), bank.checkBalance("3456456789", "2345"));
+        bank.transfer(new BigDecimal("2000"),  "3456456789", "1234567891", "2345");
+        assertEquals(new BigDecimal("1000"),  bank.checkBalance("3456456789", "2345"));
     }
 
 
