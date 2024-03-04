2e35e76bcdc756ea75c8a1870294329f28a19d07
Yisaterese <teresejosephyisa@gmail.com>
2024-03-05 00:53:31 +0100

    added code

--- src/apps/TicTacToe.java
+++ src/apps/TicTacToe.java
@@ -36,18 +36,50 @@ public class TicTacToe {
     }
 
     public void pickCell(int row, int col, CellValues cellValues) {
-        if(!isValidRowAndColumn(row, col, cellValues)) {
-           throw new IndexOutOfBoundsException("index out of bound");
+        boardCells[row][col] = cellValues;
+        if (!isValidRowAndColumn(row, col, cellValues)) {
+            throw new IndexOutOfBoundsException("index out of bound");
         }
     }
 
-    private boolean isValidRowAndColumn(int row, int col, CellValues cellValues) {
-        if (row >= 0 && row < 2 && col >= 0 && col < 2){
-            boardCells[row][col] = cellValues;
-        return true;
+    public CellValues[][] getWinByRow() {
+
+        CellValues[][] newBoardCells = new CellValues[boardCells.length][boardCells.length];
+        int counter = 0;
+        for (int row = 0; row < boardCells.length; row++) {
+            if (boardCells[row][0] == boardCells[row][1] && boardCells[row][1] == boardCells[row][2]) {
+                for (int col = 0; col < boardCells[row].length; col++) {
+                    newBoardCells[counter][col] = boardCells[row][col];
+                }
+            }
+            counter++;
+        }
+        return newBoardCells;
     }
+
+    private boolean isValidRowAndColumn(int row, int col, CellValues cellValues) {
+        if (row >= 0 && row <= 2 && col >= 0 && col <= 2) {
+            boardCells[row][col] = cellValues;
+            return true;
+        }
         throw new IndexOutOfBoundsException("Out of bound index exception");
     }
 
 
+    public boolean hasWonByColumn() {
+        for (int col = 0; col < boardCells.length; col++) {
+            if (boardCells[col][0] == boardCells[col][1] && boardCells[col][1] == boardCells[col][2]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasWonByDiagonal() {
+        for(int index = 0; index < boardCells.length; index++){
+            if(boardCells[0][2] == boardCells[1][1] && boardCells[1][1] == boardCells[2][0])
+                return true;
+        }
+        return false;
+    }
 }
--- src/apps/TicTacToeTest.java
+++ src/apps/TicTacToeTest.java
@@ -3,14 +3,13 @@ package apps;
 import org.junit.jupiter.api.Test;
 
 import static apps.CellValues.X;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TicTacToeTest {
     @Test
     public void lengthOfArrayIs3by3Test() {
         TicTacToe ticTacToe = new TicTacToe();
-        assertEquals(3, ticTacToe.getLengthOfBoardcells());
+        assertEquals(3, ticTacToe.getLengthOfBoardCells());
     }
 
     @Test
@@ -30,6 +29,50 @@ public class TicTacToeTest {
     public void checkIfCurrentIndexPicked_equalsTheValueOfTheEnumType(){
         TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.pickCell(1,2,X);
-        assertEquals(CellValues.X, ticTacToe.getValueOfBoardCells());
+        assertEquals(CellValues.X, ticTacToe.getValueOfBoardCells()[1][2]);
     }
+
+
+    @Test
+    public void checkWinByRow_itsValidWin(){
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.pickCell(1,0, X);
+        ticTacToe.pickCell(2,0, CellValues.O);
+        ticTacToe.pickCell(1,1, X);
+        ticTacToe.pickCell(2,1, CellValues.O);
+        ticTacToe.pickCell(1,2, X);
+        for(int row = 0;  row < 3; row ++) {
+            for(int col = 0; col < 3; col++) {
+                assertEquals(CellValues.O, ticTacToe.getWinByRow()[row][col]);
+            }
+        }
+
+    }
+
+    @Test
+    public void checkWinByColumn_getWin(){
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.pickCell(1,0, X);
+        ticTacToe.pickCell(2,0, CellValues.O);
+        ticTacToe.pickCell(1,1, X);
+        ticTacToe.pickCell(2,1, CellValues.O);
+        ticTacToe.pickCell(1,2, X);
+        assertTrue(ticTacToe.hasWonByColumn());
+
+    }
+
+
+    @Test
+    public void checkWinDiagonalyOnCellBoard_getWin(){
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.pickCell(1,0, X);
+        ticTacToe.pickCell(2,0, CellValues.O);
+        ticTacToe.pickCell(1,1, X);
+        ticTacToe.pickCell(2,1, CellValues.O);
+        ticTacToe.pickCell(1,2, X);
+        assertTrue(ticTacToe.hasWonByDiagonal());
+
+    }
+
+
 }
