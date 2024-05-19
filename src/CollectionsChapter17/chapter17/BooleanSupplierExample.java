a132ea8cef5bc7cfffb9deaf3a05be344d6cd4a0
Yisaterese <teresejosephyisa@gmail.com>
2024-05-19 01:16:05 +0100

    added code

--- src/CollectionsChapter17/chapter17/BooleanSupplierExample.java
+++ src/CollectionsChapter17/chapter17/BooleanSupplierExample.java
@@ -1,7 +1,18 @@
 package CollectionsChapter17.chapter17;
 
+import java.util.function.BooleanSupplier;
+
 public class BooleanSupplierExample {
     public static void main(String[] args) {
+
+        BooleanSupplier booleanSupplierExample = ()-> true;
+        System.out.println(booleanSupplierExample.getAsBoolean());
+
+        BooleanSupplier booleanSupplierExample1 = ()-> 2 > 3;
+        System.out.println(booleanSupplierExample1.getAsBoolean());
+
+        BooleanSupplier booleanSupplierExample2 = ()-> 2 * 3 == 6;
+        System.out.println(booleanSupplierExample2.getAsBoolean());
 
     }
 }
